5240ee390d1e52123776ace215ec40bf51ccef2d
Cloud User <centos@dragon.encoretel.com>
2016-11-09 08:13:13 +0000

    Releasing allip_digits_2f_authentication_signup & allip_base

---
+++ allip_base/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "All-IP Base",
+    'summary': """All-IP Base""",
+    'description': """
+      All-IP Base
+      """,
+    'author': "All-IP Cloud",
+    'website': "https://www.allipcloud.com",
+    'category': 'Extra Tools',
+    'license': 'LGPL-3',
+  #  'images': ['static/description/Two-Factor-Authenticaton.png'],
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [
+             'views/menu.xml'
+    ],
+   
+}
